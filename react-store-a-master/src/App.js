34a66dfcd43e2155a0a24d4def622413bbed40c4
Abdul Samad <abdulsamad161803@gmail.com>
2025-03-06 17:07:12 +0530

    Add files via upload

---
+++ react-store-a-master/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Products from "./components/Products";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Products />
+      <Footer/>
+    </div>
+  );
+}
+export default App;
---
+++ react-store-a-master/src/App1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App1.css"
+export default function App1() {
+    let a = 10
+    let b = 20
+  return (
+    <div>
+        {a+b}
+        <p style={{backgroundColor:'pink'}}>Hello World</p>
+        <p>Good Morning</p>
+    </div>
+  )
+}
