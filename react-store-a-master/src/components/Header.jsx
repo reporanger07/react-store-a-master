b806b1cb414066baab72fbb353ac2a391fad9710
Abdul Samad <abdulsamad161803@gmail.com>
2025-03-25 11:38:02 +0530

    Delete react-store-a-master directory

--- react-store-a-master/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import "./Header.css";
-export default function Header() {
-  return (
-    <div className="App-Header-Row">
-      <h1>My React Store</h1>
-      <div>Menu</div>
-    </div>
-  );
-}
